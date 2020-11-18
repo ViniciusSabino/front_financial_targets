48cceab2fb4914c619f4dd838f034ad59ed70347
Vinícius Rocha <vinicius.rocha@dextra-sw.com>
2020-11-18 17:08:50 -0300

    feat(pages/login): login increments

--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -10,11 +10,41 @@ const Page = styled.div`
 
 const Component = styled.div`
   background-color: #299;
-  height: 300px;
-  width: 100%;
+  height: 350px;
+  margin: auto;
+  width: 60%;
+`;
+
+const Logo = styled.div`
+  align-items: center;
+  background-color: #10C9AF;
+  display: inline-flexbox;
+  height: 100%;
+  width: 50%;
+`;
+
+const LoginForm = styled.div`
+  background-color: #909090;
+  float: right;
+  height: 100%;
+  width: 50%;
+`;
+
+const Title = styled.div`
+  color: #FFF;
+  font-size: 20px;
+  height: 40%;
+  margin: auto;
+  text-align: center;
+  text-transform: uppercase;
+  padding: 20px 0px;
+  width: 70%;
 `;
 
 export {
   Page,
   Component,
+  Logo,
+  LoginForm,
+  Title,
 };
