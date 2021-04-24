6ace9d8c20794f2801b3de4eb3cf630daba10899
Vinicius Rocha <vnc.chelsea@gmail.com>
2021-04-24 18:49:29 -0300

    refactor(components/Accounts): Login childrens components

--- src/components/Accounts/Login/Form/LoginForm.jsx
+++ src/components/Accounts/Login/Form/LoginForm.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Field from '../FormField';
+import Field from './childrens/FormField';
 import Button from '../../../UI/Button';
 
 import {
