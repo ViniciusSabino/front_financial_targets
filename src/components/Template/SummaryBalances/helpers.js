03efc5527fdc8c8c058e8323b8681b4298ce01a3
Vinicius Rocha <vnc.chelsea@gmail.com>
2021-08-12 20:18:00 -0300

    refactor(Template/SummaryBalances): in the totalBalances component the values ​​must be the sum of the current balances

--- src/components/Template/SummaryBalances/helpers.js
+++ src/components/Template/SummaryBalances/helpers.js
@@ -1,26 +1,55 @@
-import { getBalanceMainTitle } from './childrens/CurrentBalance/helpers';
-import { TYPES_OF_BALANCES } from './constants';
+/* eslint-disable no-param-reassign */
+/* eslint-disable import/prefer-default-export */
 
-const currentBalanceMapping = (balances) => {
+import { getBalanceMainTitle } from './childrens/CurrentBalance/helpers';
+import { CURRENT_BALANCE, TOTAL_BALANCE, TYPES_OF_BALANCES } from './constants';
+
+const currentBalancesMapping = (balances) => {
   const currentBalances = Object.keys(balances)
     .reduce((acc, balanceName) => [...acc, ...balances[balanceName]]
       .map((balance, index) => ({ ...balance, index })), []);
 
+  const totalBalances = currentBalances.reduce((totalBalancesAcc, balance) => {
+    const totalBalanceOptions = [TOTAL_BALANCE.TYPES.GENERAL];
+
+    if (balance.type === CURRENT_BALANCE.TYPES.INVESTMENTS) {
+      totalBalanceOptions.push(TOTAL_BALANCE.TYPES.INVESTMENTS);
+    }
+
+    totalBalanceOptions.forEach((option, index) => {
+      const indexAcc = totalBalancesAcc.findIndex((total) => total.name === option);
+
+      if (indexAcc < 0) {
+        totalBalancesAcc.push(
+          {
+            id: index,
+            index,
+            name: option,
+            value: balance.value,
+          },
+        );
+      } else {
+        totalBalancesAcc[indexAcc].value += balance.value;
+      }
+    });
+
+    return totalBalancesAcc;
+  }, []);
+
   return {
-    title: getBalanceMainTitle(TYPES_OF_BALANCES.CURRENT),
-    type: TYPES_OF_BALANCES.CURRENT,
-    balances: currentBalances,
+    currentBalances: {
+      title: getBalanceMainTitle(TYPES_OF_BALANCES.CURRENT),
+      type: TYPES_OF_BALANCES.CURRENT,
+      balances: currentBalances,
+    },
+    totalBalances: {
+      title: getBalanceMainTitle(TYPES_OF_BALANCES.TOTAL),
+      type: TYPES_OF_BALANCES.TOTAL,
+      balances: totalBalances,
+    },
   };
 };
 
-const totalBalancesMapping = (totalBalances) => ({
-  title: getBalanceMainTitle(TYPES_OF_BALANCES.TOTAL),
-  type: TYPES_OF_BALANCES.TOTAL,
-  balances: totalBalances
-    .map((totalBalance, index) => ({ ...totalBalance, index })),
-});
-
 export {
-  currentBalanceMapping,
-  totalBalancesMapping,
+  currentBalancesMapping,
 };
