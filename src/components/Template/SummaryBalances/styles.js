da2d8b0e850f63ca57b3a6845513307809ddf923
Vinícius Rocha <vnc.chelsea@gmail.com>
2021-09-08 18:48:13 -0300

    refactor

--- src/components/Template/SummaryBalances/styles.js
+++ src/components/Template/SummaryBalances/styles.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 const Component = styled.div`
-  background-color: #79AEA5;
   height: 125px;
   width: 100%;
 `;
 
 const CurrentBalances = styled.div`
+  background-color: #79AEA5;
   height: 56%;
 `;
 
